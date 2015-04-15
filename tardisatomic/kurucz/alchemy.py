c5e936d9bc66b59d19aeab1c2bb1ac05e984f652
Michael.Klauser <michael@klauser.biz>
2015-04-15 16:45:39 +0200

    added alchemy classes

--- tardisatomic/kurucz/alchemy.py
+++ tardisatomic/kurucz/alchemy.py
@@ -4,20 +4,36 @@ from sqlalchemy import Column
 
 from sqlalchemy.orm import relationship, backref
 
-from sqlalchemy import Integer, Float, String, Boolean, ForeignKey
+from sqlalchemy import Integer, Float, String, Boolean, ForeignKey, Table
 
 Base = declarative_base()
 
+level_line_association_table = Table('level_line_association', Base.metadata,
+    Column('Levels_id', Integer, ForeignKey('Levels.id')),
+    Column('Lines_id', Integer, ForeignKey('Lines.id'))
+)
+
+
 class Atoms(Base):
+    __tablename__ = "Atoms"
     id = Column(Integer, primary_key=True)
     atomic_number = Column(Integer)
+    name = Column(String(150))
+    atomic_sym = Column(String(5))
+    group = Column(Integer)
+    period = Column(Integer)
+
+    ions = relationship("Ions")
+
+
 
 
 class Ions(Base):
     __tablename__ = "Ions"
     id = Column(Integer, primary_key=True)
-    atomic_number = Column(Integer)
+    atom_id = Column(Integer, ForeignKey("Atoms.id"))
     ion_number = Column(Integer)
+    ionization_energy_at_ground_level = Column(Float)
 
     levels = relationship("Levels")
 
@@ -28,23 +44,26 @@ class Levels(Base):
     __tablename__ = "Levels"
 
     id = Column(Integer, primary_key=True)
-
-    atomic_number = Column(Integer)
-    ion_number = Column(Integer)
+    ion_id = Column(Integer, ForeignKey("Ions.id"))
     level_number = Column(Integer)
     g = Column(Integer)
     energy = Column(Float)
+    ionization_energy = Column(Float)
     label = Column(String(120))
     theoretical = Column(Boolean)
     data_source_id = Column(Integer, ForeignKey('Data_Source.id'))
     dataSource = relationship('Data_Source')
 
+
 class Lines(Base):
     __tablename__ = "Lines"
     id = Column(Integer)
-    atomic_number = Column(Integer)
-    ion_number = Column(Integer)
-    level_number = Column(Integer)
+    level_id_lower = relationship("Levels", secondary=level_line_association_table, backref="Lines")
+    level_id_upper = relationship("Levels", secondary=level_line_association_table, backref="Lines")
+    wavelength = Column(Float)
+    f_ul = Column(Float)
+    f_lu = Column(Float)
+    loggf = Column(Float)
 
 
 class DataSource(Base):
