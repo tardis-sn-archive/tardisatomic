9030a0e92cf7b7f1bf526d45a7a9698590633dff
Michael.Klauser <michael@klauser.biz>
2015-05-04 14:59:52 +0200

    db to h5

--- tardisatomic/alchemy/to_hd5/createhd5.py
+++ tardisatomic/alchemy/to_hd5/createhd5.py
@@ -1,8 +1,9 @@
-from tardisatomic.alchemy import Ion, Atom, Level,Transition, TransitionType,\
-    TransitionValue ,TransitionValueType, DataSource
+from tardisatomic.alchemy import Ion, Atom, Level, Transition, TransitionType, \
+    TransitionValue, TransitionValueType
 
-from sqlalchemy.orm import  aliased
+from sqlalchemy.orm import aliased
 import pandas as pd
+import numpy as np
 
 
 class CreateHd5(object):
@@ -33,32 +34,82 @@ class CreateHd5(object):
         trans_data = self.atomic_db.session.query(TransitionValue,
                                                   TransitionValue.value,
                                                   Transition,
+                                                  Transition.id.label(
+                                                      "transition_id"),
                                                   target_level.id.label(
                                                       "target_level_id"),
                                                   source_level.id.label(
                                                       "source_level_id"),
                                                   target_level.level_number.label(
                                                       "target_level_number"),
-                                                  source_level.level_number.label("source_level_number"),
+                                                  source_level.level_number.label(
+                                                      "source_level_number"),
+                                                  source_level.g.label(
+                                                      'g_lower'),
+                                                  target_level.g.label(
+                                                      'g_upper'),
                                                   TransitionType.name,
                                                   TransitionValueType.name
                                                   ).join(Transition).join(
             target_level,
             Transition.target_level_id == target_level.id).join(source_level,
-            Transition.target_level_id == source_level.id).join(
+                                                                Transition.target_level_id == source_level.id).join(
             TransitionType).filter(TransitionType.name == 'Line')
-        bla = self._to_data_frame(trans_data)
+
+        raw_line_transitions = self._to_data_frame(trans_data)
+        raw_line_transitions.drop('Transition', axis=1, inplace=True)
+        raw_line_transitions.drop('TransitionValue', axis=1, inplace=True)
+        raw_line_transitions_wl = raw_line_transitions.ix[raw_line_transitions[
+                                                              'name'] ==
+                                                          'wavelength']
+
+        raw_line_transitions_wl.rename(columns={'value':'wavelength'},inplace=True)
+        raw_line_transitions_loggf = raw_line_transitions.ix[
+            raw_line_transitions[
+                'name'] == 'loggf']
+        raw_line_transitions_loggf.rename(columns={'value':'loggf'},inplace=True)
+        self.line_transitions = pd.merge(raw_line_transitions_loggf[[
+            'transition_id', 'loggf']],
+                raw_line_transitions_wl,
+         on='transition_id')
+
+        self.line_transitions['f_ul'] = self.line_transitions.apply(lambda x:
+                                                                    np.power(
+                                                                        10,
+                                                                        x[
+                                                                            'loggf'])/x['g_upper'])
+        self.line_transitions['f_lu'] = self.line_transitions.apply(lambda x:
+                                                                    np.power(
+                                                                        10,
+                                                                        x[
+                                                                            'loggf'])/x['g_lower'])
+
+
+
+
+
         a = 1
         pass
-        #transition_data =
+        # transition_data =
+
+
 
 
     def _load_levels(self):
         level_data = self.atomic_db.session.query(Level, Ion, Atom).join(
-            "ion", "atom"). values(Atom.atomic_number, Ion.ion_number,
-                                   Level.level_number, Level.g, Level.energy)
+            "ion", "atom").values(Atom.atomic_number, Ion.ion_number,
+                                  Level.level_number, Level.g, Level.energy)
         self.level_data = self._to_data_frame(level_data)
 
     def _to_data_frame(self, sql_data):
         rec_data = [rec.__dict__ for rec in sql_data]
         return pd.DataFrame.from_records(rec_data)
+
+    def _to_hd5(self):
+
+        hdf = pd.HDFStore('atomic_data.h5')
+        hdf.put('levels',self.level_data, format='table', data_columns=True)
+        hdf.put('ions',self.ion_data, format='table', data_columns=True)
+        hdf.put('atoms', self.atom_data, format='table', data_columns=True)
+        hdf.close()
+
